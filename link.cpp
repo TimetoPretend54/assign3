9f7ba0cc257515533fdd369275e69cb3ab56b642
LonelyReaper <domanicwelker@yahoo.com>
2016-10-22 11:13:14 -0700

    Update link.cpp

--- link.cpp
+++ link.cpp
@@ -9,7 +9,7 @@ LinkedList::LinkedList()
 
 LinkedList::~LinkedList()
 {
-   nodeptr p = head, n;
+   nodePtr p = head, n;
    while (p != NULL)
    {
       n = p;
@@ -21,7 +21,7 @@ LinkedList::~LinkedList()
 //  Add an item to the FRONT of the list
 void LinkedList::addNode(int val)
 {
-    nodeptr n;
+    nodePtr n;
     n = new node;
     n->data = val;
     count++;
@@ -33,7 +33,7 @@ void LinkedList::addNode(int val)
     }
     else
     {
-        nodeptr tmp = head;
+        nodePtr tmp = head;
         n->next = tmp;
         head = n;
     }
@@ -41,7 +41,7 @@ void LinkedList::addNode(int val)
 
 void LinkedList::removeNode(int val)
 {
-    nodeptr prev, curr;
+    nodePtr prev, curr;
     curr = head;
     while( curr != NULL && val > curr->data )
     {
@@ -69,7 +69,7 @@ bool LinkedList::isEmpty()
 
 void LinkedList::printList()
 {
-    nodeptr p = head;
+    nodePtr p = head;
     while(p != NULL)
     {
         cout << p->data << endl;
